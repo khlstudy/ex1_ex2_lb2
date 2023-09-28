46290db1c7d959770055fba4210243c168fdafc3
nayavu <nastyaxleborob@gmail.com>
2023-09-28 11:49:00 +0300

    Add project files.

---
+++ Ar_Class_ex2_lb2/Program.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ar_Class_ex2_lb2
+{
+    public class Ar
+    {
+        private int n;
+        private int[] a;
+        private int k;
+
+        // Конструктор 1
+        public Ar(int a, int b)
+        {
+            n = (b - a + 2) / 2; // кількість парних чисел в діапазоні [a, b]
+            this.a = new int[n];
+            int value = a;
+            for (int i = 0; i < n; i++)
+            {
+                this.a[i] = value;
+                value += 2;
+            }
+            CalculateK();
+        }
+
+        // Конструктор 2
+        public Ar(string input)
+        {
+            string[] numbers = input.Split(',');
+            n = numbers.Length;
+            a = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                a[i] = int.Parse(numbers[i]);
+            }
+            CalculateK();
+        }
+
+        private void CalculateK()
+        {
+            k = 0;
+            foreach (int element in a)
+            {
+                if (element < 20)
+                    k++;
+            }
+        }
+
+        // Властивість для читання поля n
+        public int N
+        {
+            get { return n; }
+        }
+
+        // Властивість для читання властивості k
+        public int K
+        {
+            get { return k; }
+        }
+
+        // Метод для виведення масиву на екран
+        public void Print()
+        {
+            Console.WriteLine("Масив:");
+            foreach (int element in a)
+            {
+                Console.Write(element + " ");
+            }
+            Console.WriteLine();
+        }
+
+        // Метод для знаходження індексу останнього елемента, який закінчується на 0
+        public int P()
+        {
+            for (int i = n - 1; i >= 0; i--)
+            {
+                if (a[i] % 10 == 0)
+                    return i;
+            }
+            return -1;
+        }
+
+        // Метод для обчислення твору елементів масиву з індексами від i1 до i2
+        public int Pr(int i1, int i2)
+        {
+            int product = 1;
+            for (int i = i1; i <= i2; i++)
+            {
+                product *= a[i];
+            }
+            return product;
+        }
+    }
+
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            try
+            {
+                Console.WriteLine("Оберіть конструктор (1 або 2):");
+                int constructorChoice = int.Parse(Console.ReadLine());
+
+                Ar array;
+
+                if (constructorChoice == 1)
+                {
+                    Console.Write("Введіть a: ");
+                    int a = int.Parse(Console.ReadLine());
+                    Console.Write("Введіть b: ");
+                    int b = int.Parse(Console.ReadLine());
+                    array = new Ar(a, b);
+                }
+                else if (constructorChoice == 2)
+                {
+                    Console.Write("Введіть числа, розділені комою: ");
+                    string input = Console.ReadLine();
+                    string[] numbers = input.Split(',');
+
+                    int[] parsedNumbers = new int[numbers.Length];
+
+                    for (int i = 0; i < numbers.Length; i++)
+                    {
+                        if (!int.TryParse(numbers[i], out parsedNumbers[i]))
+                            throw new FormatException("Некоректні дані. Будь ласка, введіть числа, розділені комою.");
+                    }
+
+                    array = new Ar(input);
+                }
+                else
+                {
+                    throw new ArgumentException("Неправильний вибір конструктора.");
+                }
+
+                array.Print();
+                Console.WriteLine("Кількість елементів менше 20: " + array.K);
+
+                int lastIndex = array.P();
+                if (lastIndex != -1)
+                {
+                    Console.WriteLine("Індекс останнього елемента, який закінчується на 0: " + lastIndex);
+                    int productBeforeIndex = array.Pr(0, lastIndex);
+                    Console.WriteLine("Добуток елементів до цього індексу: " + productBeforeIndex);
+                }
+                else
+                {
+                    Console.WriteLine("Елемент, що закінчується на 0, не знайдено.");
+                }
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Помилка: " + ex.Message);
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine("Помилка: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Виникла невідома помилка: " + ex.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
---
+++ Circle_ex1_lb2/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Circle_ex1_lb2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            try
+            {
+                // Створення об'єкта за допомогою конструктора за замовчуванням
+                Circle circle = new Circle();
+
+                // Введення значення радіуса з клавіатури
+                Console.Write("Введіть значення радіуса: ");
+                double radiusInput = Convert.ToDouble(Console.ReadLine());
+
+                // Присвоєння значення полю через властивість
+                circle.Radius = radiusInput;
+
+                // Виведення значення поля на екран
+                circle.Print();
+
+                // Перевірка можливості існування об'єкта та виклик інших методів
+                if (circle.Correct())
+                {
+                    Console.WriteLine("Площа круга: " + circle.Area().ToString("F2"));
+                    Console.WriteLine("Довжина кола: " + circle.Length().ToString("F2"));
+                }
+                else
+                {
+                    Console.WriteLine("Коло з введеним радіусом не може існувати.");
+                }
+            }
+            catch 
+            {
+                Console.WriteLine("Виникла помилка");
+            }
+            Console.ReadLine();
+        }
+        public class Circle
+        {
+            private double radius;
+
+            // Конструктор за замовчуванням
+            public Circle()
+            {
+                radius = 0.0;
+            }
+
+            // Властивість для присвоєння значення радіусу
+            public double Radius
+            {
+                get { return radius; }
+                set { radius = value; }
+            }
+
+            // Логічний метод для перевірки чи може існувати об'єкт
+            public bool Correct()
+            {
+                return radius > 0;
+            }
+
+            // Метод для обчислення площі круга
+            public double Area()
+            {
+                return Math.PI * radius * radius;
+            }
+
+            // Метод для обчислення довжини кола
+            public double Length()
+            {
+                return 2 * Math.PI * radius;
+            }
+
+            // Метод для виведення значення поля
+            public void Print()
+            {
+                Console.WriteLine("Радіус кола: " + radius);
+            }
+        }
+    }
+}
